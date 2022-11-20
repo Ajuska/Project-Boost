ddeb22fff8507b5710de2d4ff07831cd82b5c61a
Andrea Kopecna <a.kopecna@seznam.cz>
2022-11-21 00:00:02 +0100

    Link rocket sound to rocket movement

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -6,6 +6,7 @@ public class Movement : MonoBehaviour
 {
     [SerializeField] float mainThrust = 1000f;
     [SerializeField] float rotationThurst = 100f;
+    AudioSource audioSource;
     Rigidbody rb;
 
 
@@ -13,6 +14,7 @@ public class Movement : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -24,10 +26,19 @@ public class Movement : MonoBehaviour
 
     void ProcessThrust()
     {
+
         if (Input.GetKey(KeyCode.Space))
         {
             rb.AddRelativeForce(Vector3.up * mainThrust * Time.deltaTime);
-        };
+            if (!audioSource.isPlaying)
+            {
+                audioSource.Play();
+            }
+        }
+        else
+        {
+            audioSource.Stop();
+        }
     }
 
     void ProcessRotation()
