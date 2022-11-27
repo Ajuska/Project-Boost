c08fcdfde6d5c7a9b4ef022a43c1f4bd4e295271
Andrea Kopecna <a.kopecna@seznam.cz>
2022-11-28 00:11:10 +0100

    Add two more scenes

---
+++ Assets/Scripts/Oscillator.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Oscillator : MonoBehaviour
+{
+    Vector3 startingPosition;
+    [SerializeField] Vector3 movementVector;
+
+    [SerializeField] float period = 2f; // seconds
+    float movementFactor;
+    // [SerializeField] [Range(0,1)] float movementFactor; // not needed anymore
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startingPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (period <= Mathf.Epsilon) { return; }
+        float cycles = Time.time / period; // continually growing over time
+        const float tau = Mathf.PI * 2; // constant value of 6.283
+        float rawSinWave = Mathf.Sin(cycles * tau); // going for -1 to 1
+
+        movementFactor = (rawSinWave + 1f) / 2f; // recalculated to go from 0 to 1 so it's cleaner
+
+        Vector3 offset = movementVector * movementFactor;
+        transform.position = startingPosition + offset;
+    }
+}
